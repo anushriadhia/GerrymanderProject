478ff581ce000c83a58f99a0593f8f53e74eb135
Dmitri Grozdanov <dmitri@live.unc.edu>
2017-10-01 18:19:59 -0400

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+app.listen(8080);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
