8a1ef5d316063ef79af92b417b2ead188b495633
HeyItsMedz <mehdi96@hotmail.co.uk>
2017-10-14 20:36:51 -0400

    Commented out lots of code to fit views in one page

--- public/index.js
+++ public/index.js
@@ -18,81 +18,96 @@ $(document).ready(function() {
       }
   });
 
-  var barChart = c3.generate({
-      title: {
-        text: '2016 NC Registered Voter Party Affiliation'
-      },
-      bindto: '#barChart',
-      data: {
-          // iris data from R
-          columns: [
-              ['Democrat', 57],
-              ['Republican', 33],
-              ['Independent', 10],
-          ],
-          type : 'bar',
-          onclick: function (d, i) { console.log("onclick", d, i); },
-          onmouseover: function (d, i) { console.log("onmouseover", d, i); },
-          onmouseout: function (d, i) { console.log("onmouseout", d, i); }
-      },
-      bar: {
-        width: {
-          ratio: 0.5
-        }
-      }
-  });
+  // var barChart = c3.generate({
+  //     title: {
+  //       text: '2016 NC Registered Voter Party Affiliation'
+  //     },
+  //     bindto: '#barChart',
+  //     data: {
+  //         // iris data from R
+  //         columns: [
+  //             ['Democrat', 57],
+  //             ['Republican', 33],
+  //             ['Independent', 10],
+  //         ],
+  //         type : 'bar',
+  //         onclick: function (d, i) { console.log("onclick", d, i); },
+  //         onmouseover: function (d, i) { console.log("onmouseover", d, i); },
+  //         onmouseout: function (d, i) { console.log("onmouseout", d, i); }
+  //     },
+  //     bar: {
+  //       width: {
+  //         ratio: 0.5
+  //       }
+  //     }
+  // });
 
-  var timeChart = c3.generate({
-      title: {
-        text: '2016 NC Voter Gubernatorial Preference'
-      },
-      bindto: '#timeChart',
-      data: {
-        x: 'x',
-          columns: [
-              ['x', '2016-01-01', '2016-02-01', '2016-03-01', '2016-04-01', '2016-05-01'],
-              ['McCrory', 33, 36, 37, 32, 29],
-              ['Cooper', 42, 40, 34, 39, 32],
-          ],
-      },
-      axis: {
-        x: {
-          type: 'timeseries',
-          tick: {
-            format: "%Y-%m-%d"
-          }
-        }
-      }
-
-  });
+  // var timeChart = c3.generate({
+  //     title: {
+  //       text: '2016 NC Voter Gubernatorial Preference'
+  //     },
+  //     bindto: '#timeChart',
+  //     data: {
+  //       x: 'x',
+  //         columns: [
+  //             ['x', '2016-01-01', '2016-02-01', '2016-03-01', '2016-04-01', '2016-05-01'],
+  //             ['McCrory', 33, 36, 37, 32, 29],
+  //             ['Cooper', 42, 40, 34, 39, 32],
+  //         ],
+  //     },
+  //     axis: {
+  //       x: {
+  //         type: 'timeseries',
+  //         tick: {
+  //           format: "%Y-%m-%d"
+  //         }
+  //       }
+  //     }
+  //
+  // });
 
       newMetric("lool");
 
+      newMap("Map 1");
+      newMap("Map 2");
+
 });
 
-// metrics
+// names of metrics
 var metricsList = [
-  "Reock",
-  "PP",
-  "Compactness",
-  "Voter Waste"
+
 ];
+
+// names of maps
+var mapsList = [
+
+];
+
 
 function newMetric(nameStr) {
   metricsList.push(nameStr);
   addMetricsRow(metricsList.length - 1);
-  addNavRow(metricsList.length - 1);
+
 }
 
 function addMetricsRow(metricIndex) {
 
 }
 
-function addNavRow(metricIndex) {
+
+function newMap(nameStr) {
+  mapsList.push(nameStr);
+  addNavRow(mapsList.length - 1);
+}
+
+// adds a row for a metric graph
+function addNavRow(mapIndex) {
   var $navDrawer = $('#navDrawer');
-  var newElem = '<a href="#" id="navItem' + metricsList[metricIndex] +
-    '" class="w3-bar-item w3-button w3-padding"><i class="fa fa-users fa-fw"></i>  ' + metricsList[metricIndex] + '</a>';
+
+  // html for new row
+  var newElem = '<a href="#" id="navItem' + mapsList[mapIndex] +
+    '" class="w3-bar-item w3-button w3-padding"><i class="fa fa-users fa-fw"></i>   ' + mapsList[mapIndex] + '</a>';
 
   $('#div-nav-drawer').append(newElem);
-  console.log('added');
+  console.log('Added ' + mapsList[mapIndex] + " with index " + mapIndex);
 }
