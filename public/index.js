2c254047f84f5665e1c28db13b0f93a07aafefc0
HeyItsMedz <mehdi96@hotmail.co.uk>
2017-11-01 11:21:37 -0400

    Attempt at interactivity for map

--- public/index.js
+++ public/index.js
@@ -1,3 +1,27 @@
+// names of metrics
+var metricsList = [];
+
+// names of maps
+var mapsList = [];
+
+var geojson;
+
+var info = L.control();
+
+info.onAdd = function(map) {
+  this._div = L.DomUtil.create('div', 'info');
+  this.update();
+  return this._div;
+}
+
+info.update = function(props) {
+  console.log(this);
+  console.log(this._div);
+
+  this._div.innerHTML = '<h4>Reock Score<h4>' + (props ? '<b>' + props.DISTRICT + '</b><br />' + props.Reock : 'Hover over a state');
+  info.addTo(map);
+}
+
 $(document).ready(function() {
 
   initMap();
@@ -16,11 +40,12 @@ $(document).ready(function() {
 
     // temporary
     var xmlhttp = new XMLHttpRequest();
+    var jsonMap;
     xmlhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
 
-        var myObj = JSON.parse(this.responseText);
-        L.geoJSON(myObj, {style: style}).addTo(map);  // adds geojson districts to map with style given in style()
+        jsonMap = JSON.parse(this.responseText);
+        L.geoJSON(jsonMap, {style: reockStyle}).addTo(map);  // adds geojson districts to map with style given in style()
 
       }
     };
@@ -28,10 +53,38 @@ $(document).ready(function() {
     xmlhttp.send();
     //
 
+    var $compactItem = newMap("Compactness");
+    $compactItem.click(function() {
+      clearMapItemHighlights();
+      $compactItem.addClass("w3-blue");
+
+      geojson = L.geoJSON(jsonMap, {style: compactnessStyle, onEachFeature: onEachFeature}).addTo(map);
+    });
+
+    var $reockItem = newMap('Reock');
+    $reockItem.click(function() {
+      clearMapItemHighlights();
+      $reockItem.addClass("w3-blue");
+
+      geojson = L.geoJSON(jsonMap, {style: reockStyle, onEachFeature: onEachFeature}).addTo(map);
+    });
+
   }
 
   // determines what colour a district should be, based on a given value d (compactness)
-  function getColor(d) {
+  function getCompactnessColor(d) {
+    return d > 14000 ? '#FFEDA0' :
+    d > 10000  ? '#FED976' :
+    d > 7000  ? '#FEB24C' :
+    d > 5000  ? '#FD8D3C' :
+    d > 3000   ? '#FC4E2A' :
+    d > 2000   ? '#E31A1C' :
+    d > 1000   ? '#BD0026' :
+    '#800026';
+  }
+
+  // determines what colour a district should be, based on a given value d (reock)
+  function getReockColor(d) {
     return d > 0.6 ? '#FFEDA0' :
     d > 0.55  ? '#FED976' :
     d > 0.5  ? '#FEB24C' :
@@ -40,13 +93,12 @@ $(document).ready(function() {
     d > 0.35   ? '#E31A1C' :
     d > 0.3   ? '#BD0026' :
     '#800026';
-
   }
 
   // determines how districts on map look
-  function style(feature) {
+  function compactnessStyle(feature) {
     return {
-      fillColor: getColor(feature.properties.Reock),
+      fillColor: getReockColor(feature.properties.Compactness),
       weight: 2,
       opacity: 1,
       color: 'white',
@@ -55,15 +107,52 @@ $(document).ready(function() {
     };
   }
 
-  // names of metrics
-  var metricsList = [
+  // determines how districts on map look
+  function reockStyle(feature) {
+    return {
+      fillColor: getReockColor(feature.properties.Reock),
+      weight: 2,
+      opacity: 1,
+      color: 'white',
+      dashArray: '3',
+      fillOpacity: 0.7
+    };
+  }
 
-  ];
+  function highlighFeature(e) {
+    var layer = e.target;
 
-  // names of maps
-  var mapsList = [
+    layer.setStyle({
+      weight: 5,
+      color: '#666',
+      dashArray: '',
+      fillOpacity: 0.7
+    });
 
-  ];
+    // problems with IE, Opera, and Edge will mean this function would not work
+    if(!L.Broswer.ie && !L.Browser.opera && !L.Browser.edge) {
+      layer.bringToFront();
+    }
+
+    info.update(layer.feature.properties);
+  }
+
+  function resetHighlight(e) {
+    geojson.resetStyle(e.target);
+    info.update()
+  }
+
+  function zoomToFeature(e) {
+    map.fitBounds(e.target.getBounds());
+  }
+
+  function onEachFeature(feature, layer) {
+    layer.on({
+      mouseover: highlighFeature,
+      mouseout: resetHighlight,
+      click: zoomToFeature
+    });
+  }
 
   function setWarningsText(text) {
     $('#warnings-text').text(text);
@@ -108,11 +197,17 @@ $(document).ready(function() {
   // adds a row for a metric graph
   function addNavRow(mapIndex) {
     // html for new row
-    var newElem = '<a href="#" id="navItem' + mapsList[mapIndex] +
+    var newElem = '<a href="#" id="navItem' + mapIndex +
       '" class="w3-bar-item w3-button w3-padding"><i class="fa fa-users fa-fw"></i>   ' + mapsList[mapIndex] + '</a>';
 
     $('#div-nav-drawer').append(newElem);
     console.log('Added ' + mapsList[mapIndex] + " with index " + mapIndex);
+  }
+
+  function clearMapItemHighlights() {
+    for(var i = 0; i < mapsList.length; i++) {
+      $('#navItem' + i).removeClass('w3-blue');
+    }
   }
 
 });
