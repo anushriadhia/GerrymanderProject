1f53848d282cdde9afad4b198739856ba6d95a20
Dmitri Grozdanov <dmitri@live.unc.edu>
2017-12-01 15:24:59 -0500

    Fixed merge errors

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,6 @@ var fs = require("fs");
 var content = fs.readFileSync("public/ncr.geojson");
 
 /* GET home page. */
-<<<<<<< HEAD
 router.get('/', function(req, res, next){
     res.render('home', {title: 'Spatial Gerrymandering Detection'});
 });
@@ -18,12 +17,6 @@ router.get('/map', function(req, res, next) {
     var ncrGeoText = JSON.parse(fs.readFileSync("public/ncr.geojson"));
     console.log(ncrGeoText);
     res.render('index', {var1: 10, vec1: 20, geoDump: JSON.stringify(ncrGeoText).toString() });
-=======
-router.get('/', function(req, res, next) {
-    var ncrGeoText = JSON.parse(fs.readFileSync("public/ncr.geojson"));
-    console.log(ncrGeoText);
-    res.render('index', { title: 'Geographic Gerrymandering Detection', var1: 10, vec1: 20, geoDump: JSON.stringify(ncrGeoText).toString() });
->>>>>>> ba027ae635986974b149383a1f7af46b3b2136c4
 });
 
 module.exports = router;
